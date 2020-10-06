887b8f39d80702a6a51523ef82b4c98a1efde85c
Maciej Schulc <mschulc@gmail.com>
2020-10-06 21:37:06 +0200

    Working code added

--- 4 kyu/PiramidSlideDown/PiramidSlideDown/Program.cs
+++ 4 kyu/PiramidSlideDown/PiramidSlideDown/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace PiramidSlideDown
+public class PyramidSlideDown
 {
-    class Program
+    public static int LongestSlideDown(int[][] pyramid)
     {
-        static void Main(string[] args)
+        Dictionary<string, int> way = new Dictionary<string, int>();
+        return Pyramid(pyramid, 0, 0, way);
+    }
+
+    private static int Pyramid(int[][] pyramid, int x, int y, Dictionary<string, int> way)
+    {
+        if (x >= pyramid.Length || y >= pyramid[x].Length)
         {
-            Console.WriteLine("Hello World!");
+            return 0;
         }
+
+        if (x == pyramid.Length - 1)
+        {
+            return pyramid[x][y];
+        }
+
+        string key = $"{x}:{y}";
+        if (way.ContainsKey(key))
+        {
+            return way[key];
+        }
+
+        int max = 0;
+        for (int i = 0; i < 2; i++)
+        {
+            int step = Pyramid(pyramid, x + 1, y + i, way);
+            if (step > max)
+            {
+                max = step;
+            }
+        }
+
+        way[key] = pyramid[x][y] + max;
+        return way[key];
     }
 }
