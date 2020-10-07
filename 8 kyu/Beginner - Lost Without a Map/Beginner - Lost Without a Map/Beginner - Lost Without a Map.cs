6cd952131da71bea2f72747193eb903ae87a3b32
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:56:54 +0200

    8 kyu kata added

---
+++ 8 kyu/Beginner - Lost Without a Map/Beginner - Lost Without a Map/Beginner - Lost Without a Map.cs
@@ -0,0 +1,12 @@
+﻿public class Kata
+{
+    public static int[] Maps(int[] x)
+    {
+        int[] maps = new int[x.Length];
+        for (int i = 0; i < x.Length; i++)
+        {
+            maps[i] = 2 * x[i];
+        }
+        return maps;
+    }
+}
