59c93ca5e68b4431b5a6801150a1f4e881764a2e
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:39:30 +0200

    7 kyu kata added

---
+++ 7 kyu/Sum of Minimums!/Sum of Minimums!/Sum of Minimums!.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Kata
+{
+    public static int SumOfMinimums(int[,] numbers)
+    {
+        int sum = 0;
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            int a = 0;
+            a = numbers[i, 0];
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                if (a > numbers[i, j])
+                {
+                    a = numbers[i, j];
+                }
+            }
+            sum += a;
+        }
+        return sum;
+    }
+}
