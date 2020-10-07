852332c202b693e3e4235f2d1d813a7cf5b47f7d
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:16:22 +0200

    8 kyu kata added

---
+++ 8 kyu/Reversed Strings/Reversed Strings/Reversed Strings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+public static class Kata
+{
+    public static string Solution(string str)
+    {
+        StringBuilder text = new StringBuilder();
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            text.Append(str[i]);
+        }
+        return text.ToString();
+    }
+}
