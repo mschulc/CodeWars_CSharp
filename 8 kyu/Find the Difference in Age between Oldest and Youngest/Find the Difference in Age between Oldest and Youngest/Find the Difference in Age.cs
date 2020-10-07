fab8891a3a7c1b61475006df9f3ce8d4aa58664f
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:10:08 +0200

    8 kyu kata added

---
+++ 8 kyu/Find the Difference in Age between Oldest and Youngest/Find the Difference in Age between Oldest and Youngest/Find the Difference in Age.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int[] DifferenceInAges(int[] ages)
+    {
+        int[] tab = new int[3];
+        tab[0] = ages.Min();
+        tab[1] = ages.Max();
+        tab[2] = ages.Max() - ages.Min();
+        return tab;
+    }
+}
