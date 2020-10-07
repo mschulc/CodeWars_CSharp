1cf913d20a23cb192327ff9b6bf282664de52b9f
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:28:53 +0200

    8 kyu kata added

---
+++ 8 kyu/Exclusive or (xor) Logical Operator/Exclusive or (xor) Logical Operator/Exclusive or (xor) Logical Operator.cs
@@ -0,0 +1,12 @@
+﻿public class Kata
+{
+    public static bool Xor(bool a, bool b)
+    {
+        if (a == true && b == false)
+            return true;
+        else if (b == true && a == false)
+            return true;
+        else
+            return false;
+    }
+}
