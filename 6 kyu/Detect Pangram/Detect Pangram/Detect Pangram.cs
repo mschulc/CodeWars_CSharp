4f974a2cf238e71ee6b076fb9e5cbd9828f539a5
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:24:38 +0200

    new 6 kyu kata added

---
+++ 6 kyu/Detect Pangram/Detect Pangram/Detect Pangram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class Kata
+{
+    public static bool IsPangram(string str)
+    {
+        char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        bool resault = true;
+        for (int i = 0; i < alphabet.Length; i++)
+        {
+            if (str.ToLower().Contains(alphabet[i]) == false)
+            {
+                resault = false;
+                break;
+            }
+        }
+        return resault;
+    }
+}
