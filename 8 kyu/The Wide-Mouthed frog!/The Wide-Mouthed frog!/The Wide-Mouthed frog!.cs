7c0bc8632c577fc998165ac83765505b20f6f495
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:03:57 +0200

    8 kyu kata added

---
+++ 8 kyu/The Wide-Mouthed frog!/The Wide-Mouthed frog!/The Wide-Mouthed frog!.cs
@@ -0,0 +1,10 @@
+﻿public class Kata
+{
+    public static string MouthSize(string animal)
+    {
+        if (animal.ToLower() != "alligator")
+            return "wide";
+        else
+            return "small";
+    }
+}
