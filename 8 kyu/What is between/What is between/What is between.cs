527286273b72a2578ee4f1778f4acb3bdaab510c
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:53:00 +0200

    8 kyu kata added

---
+++ 8 kyu/What is between/What is between/What is between.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Kata
+{
+    public static int[] Between(int a, int b)
+    {
+        int[] between = new int[(b - a) + 1];
+        int c = a;
+        for (int i = 0; i < between.Length; i++)
+        {
+            between[i] = c;
+            c++;
+        }
+        return between;
+    }
+}
