27c16b1de60262fb573d7033d7261158a3338b1e
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:22:52 +0200

    new 6 kyu kata added

---
+++ 6 kyu/Delete occurrences of an element if it occurs more than n times/Delete occurrences of an element if it occurs more than n times/Delete occurrences of an element if it occurs more than n times.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static int[] DeleteNth(int[] arr, int x)
+    {
+        List<int> list = arr.ToList();
+        for (int i = 1; i <= arr.Max(); i++)
+        {
+            int c = 0;
+            for (int j = 0; j < arr.Length; j++)
+            {
+                if (i == arr[j])
+                {
+                    c++;
+                    if (c > x)
+                    {
+                        list.RemoveAt(list.LastIndexOf(i));
+                    }
+                }
+            }
+        }
+        return list.ToArray();
+    }
+}
