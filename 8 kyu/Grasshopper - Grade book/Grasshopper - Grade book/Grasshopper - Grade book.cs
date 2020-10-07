0ebb2c89d0da43fc0625688d59cbbeb42cb814fe
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:37:40 +0200

    8 kyu kata  added

---
+++ 8 kyu/Grasshopper - Grade book/Grasshopper - Grade book/Grasshopper - Grade book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Kata
+{
+    public static char GetGrade(int s1, int s2, int s3)
+    {
+        int average = (s1 + s2 + s3) / 3;
+
+        if (90 <= average)
+            return 'A';
+        else if (80 <= average)
+            return 'B';
+        else if (70 <= average)
+            return 'C';
+        else if (60 <= average)
+            return 'D';
+        else
+            return 'F';
+    }
+}
