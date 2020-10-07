40d2aa70593788fb4745049e10773aaac59017dd
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:44:01 +0200

    7 kyu kata added

---
+++ 7 kyu/No oddities here/No oddities here/No oddities here.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq
+    ;
+public class NoOddities
+{
+    public static int[] NoOdds(int[] values)
+    {
+        List<int> list = new List<int>();
+        for (int i = 0; i < values.Length; i++)
+        {
+            if (values[i] % 2 == 0)
+                list.Add(values[i]);
+        }
+        return list.ToArray();
+    }
+}
