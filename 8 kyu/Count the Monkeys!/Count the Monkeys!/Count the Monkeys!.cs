baf1505b6f2c687a9ec685ea14652b450ddf069a
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:55:37 +0200

    8 kyu kata added

---
+++ 8 kyu/Count the Monkeys!/Count the Monkeys!/Count the Monkeys!.cs
@@ -0,0 +1,12 @@
+﻿public static class MonkeyCounter
+{
+    public static int[] MonkeyCount(int n)
+    {
+        int[] monkey = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            monkey[i] = i + 1;
+        }
+        return monkey;
+    }
+}
