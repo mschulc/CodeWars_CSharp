ae401151a73cd0480cb212f9e708eb2b4305efbb
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:59:46 +0200

    8 kyu kata added

---
+++ 8 kyu/Find numbers which are divisible by given number/Find numbers which are divisible by given number/Find numbers which are divisible .cs
@@ -0,0 +1,25 @@
+﻿public class Kata
+{
+    public static int[] DivisibleBy(int[] numbers, int divisor)
+    {
+        int n = 0;
+        int a = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % divisor == 0)
+            {
+                n++;
+            }
+        }
+        int[] tab = new int[n];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % divisor == 0)
+            {
+                tab[a] = numbers[i];
+                a++;
+            }
+        }
+        return tab;
+    }
+}
