9927e82fdb1005a32c83f719e9a0643e55912feb
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:28:11 +0200

    new 6 kyu kata added

---
+++ 6 kyu/Find the odd int/Find the odd int/Find the odd int.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Solution
+{
+    class Kata
+    {
+        public static int find_it(int[] seq)
+        {
+            int x = -1;
+            for (int i = 0; i <= seq.Max(); i++)
+            {
+                int c = 0;
+                for (int j = 0; j < seq.Length; j++)
+                {
+                    if (i == seq[j])
+                        c++;
+                }
+                if (c % 2 != 0)
+                    x = i;
+            }
+            return x;
+        }
+    }
+}
