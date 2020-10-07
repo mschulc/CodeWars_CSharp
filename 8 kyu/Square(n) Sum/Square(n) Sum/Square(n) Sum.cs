a01c06541b30a718d1474a3e33d4db0f44a47d56
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:05:02 +0200

    8 kyu kata added

---
+++ 8 kyu/Square(n) Sum/Square(n) Sum/Square(n) Sum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int SquareSum(int[] n)
+    {
+        double sum = 0;
+        for (int i = 0; i < n.Length; i++)
+        {
+            sum += Math.Pow(n[i], 2);
+        }
+        return (int)sum;
+    }
+}
