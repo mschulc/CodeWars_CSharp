8318301a1477b72af04f3d2cf9c21a7a75faf248
Maciej Schulc <mschulc@gmail.com>
2020-10-07 19:36:20 +0200

    New kata 7 kyu Alternate case

---
+++ 7 kyu/Alternate case/Alternate case/Alternate case.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Solution
+{
+    public static class Program
+    {
+        public static string alternateCase(string s)
+        {
+            StringBuilder text = new StringBuilder();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i].ToString() == s[i].ToString().ToUpper())
+                {
+                    text.Append(s[i].ToString().ToLower());
+                }
+                else
+                    text.Append(s[i].ToString().ToUpper());
+            }
+
+            return text.ToString();
+        }
+    }
+}
