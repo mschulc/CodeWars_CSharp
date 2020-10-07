fde0154c65012c03ee75a15d3bf45b0a7836cdbb
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:15:00 +0200

    8 kyu kata added

---
+++ 8 kyu/noobCode 01 SUPERSIZE ME or rather, this integer!/noobCode 01 SUPERSIZE ME or rather, this integer!/noobCode 01 SUPERSIZE ME.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Kata
+{
+    public static long SuperSize(long num)
+    {
+        StringBuilder score = new StringBuilder();
+        List<string> list = new List<string>();
+        string text = num.ToString();
+        for (int i = 0; i < text.Length; i++)
+        {
+            list.Add(text[i].ToString());
+        }
+        list.Sort();
+        list.Reverse();
+        for (int i = 0; i < list.Count; i++)
+        {
+            score.Append(list[i]);
+        }
+        return long.Parse(score.ToString());
+    }
+}
