521f1d6f4701aa6d1263928a26e1e52c7e91bfd0
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:31:22 +0200

    new 7 kyu kata added

---
+++ 7 kyu/Extra Perfect Numbers (Special Numbers Series 7)/Extra Perfect Numbers (Special Numbers Series 7)/Extra Perfect Numbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static int[] ExtraPerfect(int n)
+    {
+        double m = (double)n / 2;
+        if (m - (int)m != 0)
+            m += 1;
+        List<int> list = new List<int>();
+        int c = 1;
+        for (int i = 0; i < Math.Floor(m); i++)
+        {
+            list.Add(c);
+            c += 2;
+        }
+        return list.ToArray();
+    }
+}
