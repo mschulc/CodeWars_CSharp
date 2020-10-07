af9d5372a7d485f39537e67c68e3da30d716fca0
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:05:51 +0200

    new 6 kyu kata added

---
+++ 6 kyu/Consonant value/Consonant value/Consonant value.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static int Solve(string s)
+    {
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        List<int> list = new List<int>();
+        int count = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+            {
+                count = 0;
+            }
+            else
+            {
+                count = count + (alphabet.IndexOf(s[i]) + 1);
+                list.Add(count);
+            }
+        }
+        if (s is null)
+            return 0;
+        else
+            return list.Max();
+    }
+}
