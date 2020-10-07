fb84b287fbfa1d8a6b25693712504b1251b76c12
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:06:56 +0200

    8 kyu kata added

---
+++ 8 kyu/Century From Year/Century From Year/Century From Year.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public static class Kata
+{
+    public static int СenturyFromYear(int year)
+    {
+        decimal century = (decimal)year / 100;
+        if (year <= 100)
+            return 1;
+        else if (century > Math.Floor((decimal)century))
+            return (int)Math.Floor((decimal)century) + 1;
+        else
+            return (int)Math.Floor(century);
+    }
+}
