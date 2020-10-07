238d72625cff4aa021f6e74fb9524d59cb2435f8
Maciej Schulc <mschulc@gmail.com>
2020-10-07 19:45:25 +0200

    New 6 kuy kata added

---
+++ 6 kyu/How Much/How Much/How much.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Carboat
+{
+    public static string howmuch(int m, int n)
+    {
+        List<int> M = new List<int>();
+        List<int> C = new List<int>();
+        List<int> B = new List<int>();
+        int p = 0;
+        int o = 0;
+        if (n < m)
+        {
+            p = m;
+            o = n;
+        }
+        else
+        {
+            p = n;
+            o = m;
+        }
+        for (int i = Math.Abs(o); i <= Math.Abs(p); i++)
+        {
+            int c = (1 - i) / -9;
+            int b = (2 - i) / -7;
+            if (i - 7 * b == 2 && i - 9 * c == 1)
+            {
+                M.Add(i);
+                C.Add(c);
+                B.Add(b);
+            }
+        }
+        StringBuilder text = new StringBuilder();
+        text.Append("[");
+        for (int i = 0; i < M.Count; i++)
+        {
+            text.Append("[M: ");
+            text.Append(M[i].ToString());
+            text.Append(" B: ");
+            text.Append(B[i].ToString());
+            text.Append(" C: ");
+            text.Append(C[i].ToString());
+            text.Append("]");
+        }
+        text.Append("]");
+        return text.ToString();
+    }
+}
