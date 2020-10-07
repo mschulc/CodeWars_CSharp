766fdfad0fe76f2f820a675ff304bc0ecef1e9fd
Maciej Schulc <mschulc@gmail.com>
2020-10-07 19:33:26 +0200

    New kata Turkish Numbers 7 kyu

---
+++ 7 kyu/Turkish Numbers, 0-99/Turkish Numbers, 0-99/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Kata
+{
+    public static string GetTurkishNumber(int num)
+    {
+        string[] tab1 = { "sıfır", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz" };
+        string[] tab2 = { "x", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan" };
+
+        string a = num.ToString();
+
+        if (a.Length > 1 && int.Parse(a[1].ToString()) == 0)
+        {
+            return tab2[int.Parse(a[0].ToString())];
+        }
+        else if (a.Length > 1)
+        {
+            return tab2[int.Parse(a[0].ToString())] + " " + tab1[int.Parse(a[1].ToString())];
+        }
+        else
+        {
+            return tab1[int.Parse(a[0].ToString())];
+        }
+    }
+}
