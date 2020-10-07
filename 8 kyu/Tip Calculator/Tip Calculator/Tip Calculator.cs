6e32fc92faf515b63c8b41c8b1693778b4871cd2
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:51:40 +0200

    8 kyu kata added

---
+++ 8 kyu/Tip Calculator/Tip Calculator/Tip Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+    public static int CalculateTip(double amount, string rating)
+    {
+        if (rating.ToLower() == "terrible")
+            return (int)Math.Ceiling(amount * 0);
+        else if (rating.ToLower() == "poor")
+            return (int)Math.Ceiling(amount * 0.05);
+        else if (rating.ToLower() == "good")
+            return (int)Math.Ceiling(amount * 0.1);
+        else if (rating.ToLower() == "great")
+            return (int)Math.Ceiling(amount * 0.15);
+        else if (rating.ToLower() == "excellent")
+            return (int)Math.Ceiling(amount * 0.2);
+        else
+            return -1;
+    }
+}
