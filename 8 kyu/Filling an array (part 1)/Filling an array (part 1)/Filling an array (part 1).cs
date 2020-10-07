420c10472a2fd886c8a62c308502a3bc09603cdc
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:26:31 +0200

    8 kyu kata added

---
+++ 8 kyu/Filling an array (part 1)/Filling an array (part 1)/Filling an array (part 1).cs
@@ -0,0 +1,13 @@
+﻿public class Kata
+{
+    public static int[] Arr(int N)
+    {
+        int[] tab = new int[N];
+
+        for (int i = 0; i < tab.Length; i++)
+        {
+            tab[i] = i;
+        }
+        return tab;
+    }
+}
