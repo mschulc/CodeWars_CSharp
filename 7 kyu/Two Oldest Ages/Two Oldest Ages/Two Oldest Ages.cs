527ce06b88436404c84e580606ac787c775e042e
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:42:14 +0200

    7 kyu kata added

---
+++ 7 kyu/Two Oldest Ages/Two Oldest Ages/Two Oldest Ages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class LargestTwo
+{
+    public static int[] TwoOldestAges(int[] ages)
+    {
+        List<int> list = new List<int>();
+        int[] tab = new int[2];
+        for (int i = 0; i < ages.Length; i++)
+        {
+            list.Add(ages[i]);
+        }
+        tab[1] = list.Max();
+        list.Remove(list.Max());
+        tab[0] = list.Max();
+        return tab;
+    }
+}
