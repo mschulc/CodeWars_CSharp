70018251feb744d77b6b9215c796afa1f44e3e29
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:22:33 +0200

    8 kyu kata added

---
+++ 8 kyu/What's the real floor/What's the real floor/What's the real floor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int GetRealFloor(int n)
+    {
+        if (n >= 14)
+            return n - 2;
+        else if (n >= 1)
+        {
+            return n - 1;
+        }
+        else
+            return n;
+    }
+}
