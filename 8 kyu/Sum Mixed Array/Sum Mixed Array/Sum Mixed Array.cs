7e2e7139fa3412d3909e2918dd72bb38cfdc0d9d
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:06:05 +0200

    8 kyu kata added

---
+++ 8 kyu/Sum Mixed Array/Sum Mixed Array/Sum Mixed Array.cs
@@ -0,0 +1,12 @@
+﻿public class Kata
+{
+    public static int SumMix(object[] x)
+    {
+        int sum = 0;
+        for (int i = 0; i < x.Length; i++)
+        {
+            sum += int.Parse(x[i].ToString());
+        }
+        return sum;
+    }
+}
