8012280a8ef8a66d6037d49543df3a5157483c5e
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:34:47 +0200

    8 kyu kata added

---
+++ 8 kyu/N-th Power/N-th Power/N-th Power.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class Kata
+{
+    public static double Index(int[] array, int n)
+    {
+        double score = -1;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i == n)
+            {
+                score = Math.Pow(array[n], n);
+            }
+            else if (i == array.Length - 1)
+                break;
+        }
+        return score;
+    }
+}
