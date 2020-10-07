24bf5c9187fadfb0a431b7defbe261b694673351
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:39:06 +0200

    8 kyu kata added

---
+++ 8 kyu/Beginner Series 2 Clock/Beginner Series 2 Clock/Beginner Series 2 Clock.cs
@@ -0,0 +1,9 @@
+﻿using System;
+public static class Clock
+{
+    public static int Past(int h, int m, int s)
+    {
+        int count = ((((h * 60) + m) * 60) + s) * 1000;
+        return count;
+    }
+}
