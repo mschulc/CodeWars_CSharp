a80c267ed4b76679496f1730187c20a5eaa73a97
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:25:30 +0200

    8 kyu kata added

---
+++ 8 kyu/Holiday VI - Shark Pontoon/Holiday VI - Shark Pontoon/Holiday VI - Shark Pontoon.cs
@@ -0,0 +1,34 @@
+﻿public class Kata
+{
+    public static string Shark(
+      int pontoonDistance,
+      int sharkDistance,
+      int yourSpeed,
+      int sharkSpeed,
+      bool dolphin)
+    {
+        double me = (double)pontoonDistance / (double)yourSpeed;
+        double shark = 0;
+
+        if (dolphin == true)
+        {
+            shark = (double)sharkDistance / (double)(sharkSpeed / 2);
+        }
+        else if (dolphin == false)
+        {
+            shark = (double)sharkDistance / (double)sharkSpeed;
+        }
+        if (sharkDistance == 0)
+            return "Shark Bait!";
+        else if (pontoonDistance == 0)
+            return "Alive!";
+        else if (shark < me)
+        {
+            return "Shark Bait!";
+        }
+        else
+        {
+            return "Alive!";
+        }
+    }
+}
