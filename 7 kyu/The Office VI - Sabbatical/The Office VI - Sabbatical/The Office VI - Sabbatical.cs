e9b5c03d7de6b7f36f433196cd6209eecbbc15fb
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:38:08 +0200

    7 kyu kata added

---
+++ 7 kyu/The Office VI - Sabbatical/The Office VI - Sabbatical/The Office VI - Sabbatical.cs
@@ -0,0 +1,19 @@
+﻿public class Kata
+{
+    public static string Sabb(string x, int val, int happ)
+    {
+        int letter = 0;
+        string y = x.ToLower();
+        for (int i = 0; i < y.Length; i++)
+        {
+            if (y[i] == 's' || y[i] == 'a' || y[i] == 'b' || y[i] == 't' || y[i] == 'i' || y[i] == 'c' || y[i] == 'l')
+                letter++;
+        }
+        int sum = letter + val + happ;
+
+        if (sum > 22)
+            return "Sabbatical! Boom!";
+        else
+            return "Back to your desk, boy.";
+    }
+}
