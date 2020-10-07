78f42683064c2566a6378d1996659c22cadf4195
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:21:21 +0200

    8 kyu kata added

---
+++ 8 kyu/Array plus array/Array plus array/Array plus array.cs
@@ -0,0 +1,16 @@
+﻿public static class Kata
+{
+    public static int ArrayPlusArray(int[] arr1, int[] arr2)
+    {
+        int score = 0;
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            score += arr1[i];
+        }
+        for (int i = 0; i < arr2.Length; i++)
+        {
+            score += arr2[i];
+        }
+        return score;
+    }
+}
