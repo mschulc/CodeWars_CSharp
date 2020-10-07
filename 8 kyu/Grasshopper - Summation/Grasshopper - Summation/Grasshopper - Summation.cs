5bd50957936db4b5073714ec814baf531fde7102
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:32:25 +0200

    8 kyu kata added

---
+++ 8 kyu/Grasshopper - Summation/Grasshopper - Summation/Grasshopper - Summation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int summation(int num)
+    {
+        int score = 0;
+        for (int i = 1; i <= num; i++)
+        {
+            score += i;
+        }
+        return score;
+    }
+}
