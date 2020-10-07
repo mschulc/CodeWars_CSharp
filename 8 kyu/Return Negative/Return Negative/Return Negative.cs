93407820f8de2f4728f0dfbf717e38ccc593584e
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:17:21 +0200

    8 kyu kata added

---
+++ 8 kyu/Return Negative/Return Negative/Return Negative.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int MakeNegative(int number)
+    {
+        if (number > 0)
+            return number * -1;
+        else if (number < 0)
+            return number;
+        else
+            return 0;
+    }
+}
