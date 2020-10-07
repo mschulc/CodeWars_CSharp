1eb92a706337dd5ae6d9d1923caa0db0966fb2ca
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:20:29 +0200

    8 kyu kata added

---
+++ 8 kyu/Beginner Series 4 Cockroach/Beginner Series 4 Cockroach/Beginner Series 4 Cockroach.cs
@@ -0,0 +1,9 @@
+﻿using System;
+public class Cockroach
+{
+    public static int CockroachSpeed(double x)
+    {
+        double score = x * 1000 / 36;
+        return (int)Math.Floor(score);
+    }
+}
