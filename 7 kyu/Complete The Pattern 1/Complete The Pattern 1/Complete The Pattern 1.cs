491ba1422afa74e71ba34419f20608ff38357b08
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:32:54 +0200

    7 kyu kata added

---
+++ 7 kyu/Complete The Pattern 1/Complete The Pattern 1/Complete The Pattern 1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+public class Kata
+{
+    public string Pattern(int n)
+    {
+        StringBuilder text = new StringBuilder();
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                text.Append(i);
+            }
+            if (i != n)
+                text.Append("\n");
+        }
+        return text.ToString();
+    }
+}
