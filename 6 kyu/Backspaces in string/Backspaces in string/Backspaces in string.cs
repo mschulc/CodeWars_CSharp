5bca23bd334a1f1aa316c166efc838f9954e1a66
Maciej Schulc <mschulc@gmail.com>
2020-10-07 19:51:09 +0200

    new 6 kyu kata added

---
+++ 6 kyu/Backspaces in string/Backspaces in string/Backspaces in string.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+public class Kata
+{
+    public static string CleanString(string s)
+    {
+        List<char> list = s.ToList();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (list[i] == '#')
+            {
+                list.RemoveAt(i);
+                if (i != 0)
+                {
+                    list.RemoveAt(i - 1);
+                }
+                i = -1;
+            }
+        }
+        foreach (var item in list)
+        {
+            text.Append(item);
+        }
+        return text.ToString();
+    }
+}
