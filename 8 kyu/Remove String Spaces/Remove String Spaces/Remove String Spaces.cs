49bf1e65a9a519390cad5989b1966c83f5d34859
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:48:27 +0200

    8  kyu kata added

---
+++ 8 kyu/Remove String Spaces/Remove String Spaces/Remove String Spaces.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace Solution
+{
+    public static class SpacesRemover
+    {
+        public static string NoSpace(string input)
+        {
+            StringBuilder text = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != ' ')
+                {
+                    text.Append(input[i]);
+                }
+            }
+            return text.ToString();
+        }
+    }
+}
