0c1e49cbf4458378f30b41fa5fcab44bd9183c7b
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:07:51 +0200

    8 kyu kata added

---
+++ 8 kyu/Remove First and Last Character/Remove First and Last Character/Remove First and Last Character.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+public class Kata
+{
+    public static string Remove_char(string s)
+    {
+        StringBuilder text = new StringBuilder();
+
+        for (int i = 1; i < s.Length - 1; i++)
+        {
+            text.Append(s[i]);
+        }
+        return text.ToString();
+    }
+}
