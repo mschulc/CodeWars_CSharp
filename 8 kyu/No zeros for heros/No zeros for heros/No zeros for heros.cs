ece318fdd19d9ff84b892304eaf24c7070bf1887
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:30:47 +0200

    8 kyu kata added

---
+++ 8 kyu/No zeros for heros/No zeros for heros/No zeros for heros.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+public class NoBoring
+{
+    public static int NoBoringZeros(int n)
+    {
+        StringBuilder text = new StringBuilder();
+        text.Append(n);
+        List<string> lista = new List<string>();
+        for (int i = 0; i < text.ToString().Length; i++)
+        {
+            lista.Add(text[i].ToString());
+        }
+        for (int i = lista.Count - 1; i >= 0; i--)
+        {
+            if (lista[0] == "0")
+                lista.Add("0");
+            else if (lista[i] == "0")
+            {
+                lista.RemoveAt(i);
+            }
+            else if (lista[i] != "0")
+                break;
+        }
+        text.Clear();
+        for (int i = 0; i < lista.Count; i++)
+        {
+            text.Append(lista[i]);
+        }
+        return int.Parse(text.ToString());
+    }
+}
