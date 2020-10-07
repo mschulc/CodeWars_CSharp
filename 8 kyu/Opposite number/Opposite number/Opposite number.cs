1b2a274d0de6112f61021de73e996503ab871dc1
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:23:32 +0200

    8 kyu kata added

---
+++ 8 kyu/Opposite number/Opposite number/Opposite number.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Kata
+{
+    public static int Opposite(int number)
+    {
+        if (number == 0)
+            return 0;
+        else
+            return number - (number * 2);
+    }
+}
