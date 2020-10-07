87d590c0f120f8b262e2bb7ecb7fa10afa2dd772
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:27:22 +0200

    8 kyu kata added

---
+++ 8 kyu/How good are you really/How good are you really/How good are you really.cs
@@ -0,0 +1,16 @@
+﻿public class Kata
+{
+    public static bool BetterThanAverage(int[] ClassPoints, int YourPoints)
+    {
+        int score = 0;
+        for (int i = 0; i < ClassPoints.Length; i++)
+        {
+            score += ClassPoints[i];
+        }
+        double avarage = score / ClassPoints.Length;
+        if (avarage > YourPoints)
+            return false;
+        else
+            return true;
+    }
+}
