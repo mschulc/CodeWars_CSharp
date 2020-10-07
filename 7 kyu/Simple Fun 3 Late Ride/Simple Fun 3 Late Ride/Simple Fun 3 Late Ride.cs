cdfc5fae17ab4eddfbf5f7eb522a5e59ccd01ade
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:37:03 +0200

    7 kyu kata added

---
+++ 7 kyu/Simple Fun 3 Late Ride/Simple Fun 3 Late Ride/Simple Fun 3 Late Ride.cs
@@ -0,0 +1,31 @@
+﻿namespace myjinxin
+{
+    using System;
+    using System.Text;
+
+    public class Kata
+    {
+        public int LateRide(int n)
+        {
+            StringBuilder text = new StringBuilder();
+            int m = n;
+            int h = 0;
+            if (n > 60)
+            {
+                do
+                {
+                    m -= 60;
+                    h++;
+                } while (m >= 60);
+            }
+            text.Append(h);
+            text.Append(m);
+            int sum = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                sum += int.Parse(text[i].ToString());
+            }
+            return sum;
+        }
+    }
+}
