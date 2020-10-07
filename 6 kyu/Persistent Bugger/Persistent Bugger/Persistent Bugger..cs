69ca0bb3ba9dd631ca6d22378e4da723aeec692a
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:26:20 +0200

    new 6 kyu kata added

---
+++ 6 kyu/Persistent Bugger/Persistent Bugger/Persistent Bugger..cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Persist
+{
+    public static int Persistence(long n)
+    {
+        string g = n.ToString();
+        int c = 0;
+        while (g.Length > 1)
+        {
+            long x = 1;
+            for (int i = 0; i < g.Length; i++)
+            {
+                x *= long.Parse(g[i].ToString());
+            }
+            g = x.ToString();
+            c++;
+        }
+        return c;
+    }
+}
