d461a96d0dae28108da9ca34bb738d16071243f9
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:18:24 +0200

    8 kyu kata added

---
+++ 8 kyu/Beginner Series 1 School Paperwork/Beginner Series 1 School Paperwork/Beginner Series 1 School Paperwork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public static class Paper
+{
+    public static int Paperwork(int n, int m)
+    {
+        if (n <= 0 || m <= 0)
+            return 0;
+        else
+            return n * m;
+    }
+}
