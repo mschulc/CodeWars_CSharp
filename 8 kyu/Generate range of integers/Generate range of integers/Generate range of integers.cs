3ef18bc1cac0a54fb933996afeb288cc738643bf
Maciej Schulc <mschulc@gmail.com>
2020-10-07 21:01:57 +0200

    8 kyu kata added

---
+++ 8 kyu/Generate range of integers/Generate range of integers/Generate range of integers.cs
@@ -0,0 +1,17 @@
+﻿public class Kata
+{
+    public static int[] GenerateRange(int min, int max, int step)
+    {
+        int count = ((max - min) / step) + 1;
+        int[] tab = new int[count];
+
+        int s = min;
+        tab[0] = min;
+        for (int i = 1; i < count; i++)
+        {
+            s += step;
+            tab[i] = s;
+        }
+        return tab;
+    }
+}
