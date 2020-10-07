f091cf834fc42f79bcd7d13ca852b5560cdde53e
Maciej Schulc <mschulc@gmail.com>
2020-10-07 20:50:12 +0200

    8 kyu kata added

---
+++ 8 kyu/Draw stairs/Draw stairs/Draw stairs.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+public class Kata
+{
+    public static string DrawStairs(int n)
+    {
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < n; i++)
+        {
+            text.Append("I");
+            if (i < n - 1)
+            {
+                text.Append("\n");
+                for (int j = i; j >= 0; j--)
+                {
+                    text.Append(" ");
+                }
+            }
+        }
+        if (n == 1)
+            return "I";
+        else
+            return text.ToString();
+    }
+}
