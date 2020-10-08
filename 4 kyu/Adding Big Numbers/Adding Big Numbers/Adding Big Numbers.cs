8a918805e47dc35fe95f737c63a4cbc63d2b497d
Maciej Schulc <mschulc@gmail.com>
2020-10-08 19:46:18 +0200

    4 kyu kata added

---
+++ 4 kyu/Adding Big Numbers/Adding Big Numbers/Adding Big Numbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Numerics;
+
+public class Kata
+{
+    public static string Add(string a, string b)
+    {
+        BigInteger sum;
+        BigInteger.TryParse(a, out BigInteger x);
+        BigInteger.TryParse(b, out BigInteger y);
+
+        sum = x + y;
+        return sum.ToString();
+    }
+}
