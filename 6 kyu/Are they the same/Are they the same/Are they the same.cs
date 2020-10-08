f03edd33e1b6eaad8229f848bf2889b1d0bb240f
Maciej Schulc <mschulc@gmail.com>
2020-10-08 19:40:47 +0200

    6 kyu kata added

---
+++ 6 kyu/Are they the same/Are they the same/Are they the same.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+class AreTheySame
+{
+    public static bool comp(int[] a, int[] b)
+    {
+        bool comp = true;
+        if (a is null || b is null)
+        {
+            comp = false;
+        }
+        else if (a.Length != b.Length)
+        {
+            comp = false;
+        }
+        else
+        {
+            Array.Sort(a);
+            Array.Sort(b);
+        }
+
+        for (int i = 0; i < a.Length - 1; i++)
+        {
+            if (int.Parse(b[i].ToString()) != (int)Math.Pow(int.Parse(a[i].ToString()), 2))
+            {
+                comp = false;
+                break;
+
+            }
+        }
+        return comp;
+    }
+}
