a05dc55f887969e09ac9c58fc80619ef24392a79
Maciej Schulc <mschulc@gmail.com>
2020-10-08 19:19:27 +0200

    8 kyu kata added

---
+++ 8 kyu/Watermelon/Watermelon/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class Watermelon
+{
+    public static bool Divide(int weight)
+    {
+        if (weight > 3 && weight % 2 == 0)
+            return true;
+        else
+            return false;
+    }
+}
