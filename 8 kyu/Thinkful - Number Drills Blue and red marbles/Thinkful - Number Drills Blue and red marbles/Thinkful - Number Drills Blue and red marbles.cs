34eb405a79920e3c017a7f9005ab166da9348ea8
Maciej Schulc <mschulc@gmail.com>
2020-10-08 19:28:49 +0200

    8 kyu kata added

---
+++ 8 kyu/Thinkful - Number Drills Blue and red marbles/Thinkful - Number Drills Blue and red marbles/Thinkful - Number Drills Blue and red marbles.cs
@@ -0,0 +1,8 @@
+﻿public static class BlueAndRedMarbles
+{
+    public static double GuessBlue(uint blueStart, uint redStart, uint bluePulled, uint redPulled)
+    {
+        double count = ((double)blueStart - (double)bluePulled) / (((double)blueStart - (double)bluePulled) + ((double)redStart - (double)redPulled));
+        return count;
+    }
+}
