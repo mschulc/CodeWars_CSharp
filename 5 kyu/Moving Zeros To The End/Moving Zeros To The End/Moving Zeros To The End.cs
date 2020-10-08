5d4f294b4854d8ab9332811c30075fe0dd2a9e67
Maciej Schulc <mschulc@gmail.com>
2020-10-08 19:53:07 +0200

    5 kyu kata added

---
+++ 5 kyu/Moving Zeros To The End/Moving Zeros To The End/Moving Zeros To The End.cs
@@ -0,0 +1,14 @@
+﻿public class Kata
+{
+    public static int[] MoveZeroes(int[] arr)
+    {
+        int[] tab = new int[arr.Length];
+        int c = 0;
+        foreach (int element in arr)
+        {
+            if (element != 0)
+                tab[c++] = element;
+        }
+        return tab;
+    }
+}
