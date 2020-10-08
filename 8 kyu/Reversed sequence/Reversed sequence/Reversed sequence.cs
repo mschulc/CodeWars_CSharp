59ef8f513826dee5f3e28030c68b4a3f3fd22eb8
Maciej Schulc <mschulc@gmail.com>
2020-10-08 20:08:05 +0200

    8 kyu kata added

---
+++ 8 kyu/Reversed sequence/Reversed sequence/Reversed sequence.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int[] ReverseSeq(int n)
+    {
+        int[] tab = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            tab[i] = n - i;
+        }
+        return tab;
+    }
+}
