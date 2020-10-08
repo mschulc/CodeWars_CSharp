c512d2e064da69b68327bd69f6b5c7ea83d3181b
Maciej Schulc <mschulc@gmail.com>
2020-10-08 19:44:30 +0200

    6 kyu kata added

---
+++ 6 kyu/Create Phone Number/Create Phone Number/Create Phone Number.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+public class Kata
+{
+    public static string CreatePhoneNumber(int[] numbers)
+    {
+        StringBuilder pnumber = new StringBuilder();
+        pnumber.Append("(");
+        for (int i = 0; i <= 2; i++)
+        {
+            pnumber.Append(numbers[i]);
+        }
+        pnumber.Append(") ");
+        for (int i = 3; i <= 5; i++)
+        {
+            pnumber.Append(numbers[i]);
+        }
+        pnumber.Append("-");
+        for (int i = 6; i <= 9; i++)
+        {
+            pnumber.Append(numbers[i]);
+        }
+        return pnumber.ToString();
+    }
+}
