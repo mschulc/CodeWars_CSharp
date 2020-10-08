b029714a2243dbd4db7ce4232326739c262d2b06
Maciej Schulc <mschulc@gmail.com>
2020-10-08 19:51:46 +0200

    5 kyu kata added

---
+++ 5 kyu/Number of trailing zeros of N!/Number of trailing zeros of N!/Number of trailing zeros of N!.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int TrailingZeros(int n)
+    {
+        int result = 0;
+        for (int i = 5; n / i >= 1; i *= 5)
+            result += n / i;
+        return result;
+    }
+}
