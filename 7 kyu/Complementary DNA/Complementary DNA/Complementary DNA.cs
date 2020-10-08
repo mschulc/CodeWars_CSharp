132d4aacfa6540b871d0c24078a8d6186a515bf0
Maciej Schulc <mschulc@gmail.com>
2020-10-08 19:59:11 +0200

    7 kyu kata added

---
+++ 7 kyu/Complementary DNA/Complementary DNA/Complementary DNA.cs
@@ -0,0 +1,28 @@
+﻿public class DnaStrand
+{
+    public static string MakeComplement(string dna)
+    {
+        string proper_dna = "";
+        for (int i = 0; i < dna.Length; i++)
+        {
+
+            if (dna[i] == 'A')
+            {
+                proper_dna = proper_dna + "T";
+            }
+            else if (dna[i] == 'T')
+            {
+                proper_dna = proper_dna + "A";
+            }
+            else if (dna[i] == 'C')
+            {
+                proper_dna = proper_dna + "G";
+            }
+            else if (dna[i] == 'G')
+            {
+                proper_dna = proper_dna + "C";
+            }
+        }
+        return proper_dna;
+    }
+}
