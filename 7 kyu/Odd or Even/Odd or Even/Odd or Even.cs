0ffd67372de8a238d76013ffa62b5b10802eae0a
Maciej Schulc <mschulc@gmail.com>
2020-10-08 20:09:09 +0200

    7 kyu kata added

---
+++ 7 kyu/Odd or Even/Odd or Even/Odd or Even.cs
@@ -0,0 +1,19 @@
+﻿public class Kata
+{
+    public static string OddOrEven(int[] array)
+    {
+        int vec = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            vec += array[i];
+        }
+        if (vec % 2 == 0)
+        {
+            return "even";
+        }
+        else
+        {
+            return "odd";
+        }
+    }
+}
