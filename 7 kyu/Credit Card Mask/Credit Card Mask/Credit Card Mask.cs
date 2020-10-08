1784dff7cb0edce4f3bec88c30d048a3ee962624
Maciej Schulc <mschulc@gmail.com>
2020-10-08 20:06:23 +0200

    7 kyu kata added

---
+++ 7 kyu/Credit Card Mask/Credit Card Mask/Credit Card Mask.cs
@@ -0,0 +1,19 @@
+﻿public static class Kata
+{
+    public static string Maskify(string cc)
+    {
+        string nap = "";
+        for (int i = 0; i < cc.Length; i++)
+        {
+            if (i < cc.Length - 4)
+            {
+                nap = nap + "#";
+            }
+            else if (i >= cc.Length - 4)
+            {
+                nap = nap + cc[i];
+            }
+        }
+        return nap;
+    }
+}
