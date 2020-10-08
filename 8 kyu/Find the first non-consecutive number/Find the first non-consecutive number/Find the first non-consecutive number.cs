0dd79833b0f3016a90cb4e33d2de7890bc8382c0
Maciej Schulc <mschulc@gmail.com>
2020-10-08 19:25:57 +0200

    8 kyu kata added

---
+++ 8 kyu/Find the first non-consecutive number/Find the first non-consecutive number/Find the first non-consecutive number.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+public class Kata
+{
+    public static object FirstNonConsecutive(int[] arr)
+    {
+        List<int> lista = new List<int>();
+        if (arr.Length > 0)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i >= arr.Length - 1)
+                    return null;
+                else if (arr[i] != arr[i + 1] - 1)
+                {
+                    lista.Add(arr[i + 1]);
+                    break;
+                }
+            }
+            if (lista.Count != 0)
+                return lista[0];
+            else
+                return null;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
