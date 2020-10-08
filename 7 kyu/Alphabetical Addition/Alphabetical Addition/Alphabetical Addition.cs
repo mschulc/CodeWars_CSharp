9ab12dee2f6692fc107d7303eb3f7c0b5d06d35e
Maciej Schulc <mschulc@gmail.com>
2020-10-08 19:37:04 +0200

    7 kyu kata added

---
+++ 7 kyu/Alphabetical Addition/Alphabetical Addition/Alphabetical Addition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Kata
+{
+    public static char AddLetters(char[] letters)
+    {
+        char[] alphabet = { 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y' };
+        int count = 0;
+        for (int i = 0; i < letters.Length; i++)
+        {
+            for (int j = 0; j < alphabet.Length; j++)
+            {
+                if (letters[i] == alphabet[j])
+                {
+                    count += j;
+                    if (count >= alphabet.Length)
+                    {
+                        count -= alphabet.Length;
+                    }
+                }
+            }
+        }
+        if (letters is null)
+        {
+            return alphabet[0];
+        }
+        else
+        {
+            return alphabet[count];
+        }
+    }
+}
