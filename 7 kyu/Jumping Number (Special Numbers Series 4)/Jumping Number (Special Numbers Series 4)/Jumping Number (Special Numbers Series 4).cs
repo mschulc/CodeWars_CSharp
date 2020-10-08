8eed6dc55d18db415dfc497f78b8c974faf0c326
Maciej Schulc <mschulc@gmail.com>
2020-10-08 19:38:24 +0200

    7 kyu kata added

---
+++ 7 kyu/Jumping Number (Special Numbers Series 4)/Jumping Number (Special Numbers Series 4)/Jumping Number (Special Numbers Series 4).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+
+class Kata
+{
+    public static string JumpingNumber(BigInteger number)
+    {
+        string digits = number.ToString();
+        string result = "Not!!";
+        if (digits.Length == 1)
+        {
+            result = "Jumping!!";
+        }
+        else
+        {
+            for (int i = 1; i < digits.Length; i++)
+            {
+                int wynik = int.Parse(digits[i - 1].ToString()) - int.Parse(digits[i].ToString());
+                if (Math.Abs(wynik) != 1)
+                {
+                    result = "Not!!";
+                    break;
+                }
+                else
+                {
+                    result = "Jumping!!";
+                }
+            }
+        }
+        return result;
+    }
+}
