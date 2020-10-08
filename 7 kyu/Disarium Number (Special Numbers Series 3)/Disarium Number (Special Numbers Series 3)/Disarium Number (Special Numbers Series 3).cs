28b7a5da8ae2ea3958fec4f315cd1125539d0017
Maciej Schulc <mschulc@gmail.com>
2020-10-08 19:47:53 +0200

    7 kyu kata added

---
+++ 7 kyu/Disarium Number (Special Numbers Series 3)/Disarium Number (Special Numbers Series 3)/Disarium Number (Special Numbers Series 3).cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Kata
+{
+    public static string DisariumNumber(int number)
+    {
+        string x = number.ToString();
+        double result = 0;
+        for (int j = 0; j < x.Length; j++)
+        {
+            result = result + Math.Pow(int.Parse(x[j].ToString()), (j + 1));
+        }
+        if ((int)result == number)
+        {
+            return "Disarium !!";
+        }
+        else
+        {
+            return "Not !!";
+        }
+    }
+}
