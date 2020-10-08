93013f36d0d11a5bcd10d86119c1d4b48cf8850d
Maciej Schulc <mschulc@gmail.com>
2020-10-08 19:35:42 +0200

    8 kyu kata added

---
+++ 8 kyu/Convert number to reversed array of digits/Convert number to reversed array of digits/Convert number to reversed array of digits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solution
+{
+    class Digitizer
+    {
+        public static long[] Digitize(long n)
+        {
+            string numer = n.ToString();
+            long[] tab = new long[numer.Length];
+            for (int i = 0; i < numer.Length; i++)
+            {
+                tab[i] = long.Parse(numer[i].ToString());
+            }
+            Array.Reverse(tab);
+            return tab;
+        }
+    }
+}
