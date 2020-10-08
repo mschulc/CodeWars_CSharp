bd129b0a210d64ac3167be2ee7669b4c14d8d2b6
Maciej Schulc <mschulc@gmail.com>
2020-10-08 19:57:47 +0200

    6 kyu kata added

---
+++ 6 kyu/Christmas tree/Christmas tree/Christmas tree.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System;
+
+public class ChristmasTreeGenerator
+{
+    public static string ChristmasTree(int height)
+    {
+        char star = '*';
+        char field = ' ';
+        StringBuilder tree = new StringBuilder();
+        if (height == 0)
+        {
+            return "";
+        }
+        else if (height == 1)
+        {
+            return "*";
+        }
+        else
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int o = height - 1; o > i; o--)
+                {
+                    tree.Append(field);
+                }
+                for (int j = 0; j < (i + 1) + i; j++)
+                {
+
+                    tree.Append(star);
+                }
+                for (int o = height - 1; o > i; o--)
+                {
+                    tree.Append(field);
+                }
+                if (i < height - 1)
+                    tree.Append("\n");
+            }
+            return tree.ToString();
+        }
+    }
+}
